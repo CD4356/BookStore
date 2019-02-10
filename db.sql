3160c698b1793d64d15395866a18e2048ba0f1aa
CD4356 <46418590+CD4356@users.noreply.github.com>
2019-02-10 14:50:02 +0800

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.53 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `book` (
+	`id` varchar (30),
+	`name` varchar (60),
+	`price` double ,
+	`number` int (11)
+); 
+insert into `book` (`id`, `name`, `price`, `number`) values('101','红楼梦','82','3');
+insert into `book` (`id`, `name`, `price`, `number`) values('102','西游记','63','20');
+insert into `book` (`id`, `name`, `price`, `number`) values('103','水浒传','95.8','15');
+insert into `book` (`id`, `name`, `price`, `number`) values('104','三国演义','108','9');
